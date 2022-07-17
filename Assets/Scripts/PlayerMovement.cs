ddc86e8671de7022264aca23f54ed94c3dfd1c29
TienVanTheTen <107867446+TienVanTheTen@users.noreply.github.com>
2022-07-17 11:24:32 +0200

    Sprinting and cleanup code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,56 +4,101 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("keybinds")]
+    public KeyCode SprintKey = KeyCode.LeftShift;
+    public KeyCode JumpKey = KeyCode.Space;
 
     public CharacterController characterController;
 
 
     public float gravity = -9.81f;
-    public float speed = 12f;
+    private float Speed;
+    public float WalkingSpeed;
+    public float SprintSpeed;
     public float JumpHeight = 3f; 
 
     public Transform GroundCheck;
     public float GroundDisstance = 0.4f;
     public LayerMask GroundMask;
 
+    bool isGrounded;
 
     Vector3 velocity;
 
-    bool isGrounded;
-    // Start is called before the first frame update
-    void Start()
+    
+
+    public MovementState stateplayer;
+    public enum MovementState
     {
-        
+        Walking,
+        Sprinting,
+        Air,
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void StateHandler()
+    {
+        //mode - sprinting
+        if (Input.GetKey(SprintKey) && isGrounded)
+        {
+            stateplayer = MovementState.Sprinting;
+            Speed = SprintSpeed;
+        }
+        else if (isGrounded)
+        {
+            stateplayer = MovementState.Walking;
+            Speed = WalkingSpeed;
+        }
+        else
+        {
+            stateplayer = MovementState.Air;
+        }
+    }
+    
+    void MovementPlayer()
     {
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * Speed * Time.deltaTime);
+
+        //jumping
+        if (Input.GetKey(JumpKey) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+
+    }
+
+    void GroundChecks()
+    {
         isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDisstance, GroundMask);
         //checking if you are falling
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-
-
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        
-        characterController.Move(move * speed * Time.deltaTime);
-
-        //jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
-        }
-
         //falling speed
         velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);    
+        characterController.Move(velocity * Time.deltaTime);
+
+    }
+    // Update is called once per frame
+    
+    
+    
+    
+    
+    
+    void FixedUpdate()
+    {
+              
+        StateHandler();
+        MovementPlayer();
+        GroundChecks();
+                 
     }
 }
