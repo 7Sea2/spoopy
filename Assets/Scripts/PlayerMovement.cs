5d52df8825d8bc3c3c5f1fc993bbeccf18486f7a
TienVanTheTen <107867446+TienVanTheTen@users.noreply.github.com>
2022-07-18 16:37:46 +0200

    player movement ong klaar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,35 @@ public class PlayerMovement : MonoBehaviour
     [Header("keybinds")]
     public KeyCode SprintKey = KeyCode.LeftShift;
     public KeyCode JumpKey = KeyCode.Space;
+    public KeyCode CrouchKey = KeyCode.LeftControl;
+
 
     public CharacterController characterController;
 
     [Header ("physics")]
-    public float gravity = -9.81f;
+    public float gravity;
     private float Speed;
     public float WalkingSpeed;
     public float SprintSpeed;
 
-    
+    Rigidbody rb;
 
     public float JumpHeight = 3f;
     bool isGrounded;
+    bool isCeiling = false;
+    public float CeilingDisstance = 0.4f;
+    public Transform CeilingCheck;
     public Transform GroundCheck;
     public float GroundDisstance = 0.4f;
     public LayerMask GroundMask;
 
-    
+
+    [Header("Crouching")]
+    public float CrouchSpeed;
+    public float CrouchYScale;
+    private float StartYScale;
+
+
 
     Vector3 velocity;
 
@@ -35,14 +46,41 @@ public class PlayerMovement : MonoBehaviour
     {
         Walking,
         Sprinting,
+        Crouching,
         Air,
     }
 
-
+    private void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        StartYScale = transform.localScale.y;
+    }
     private void StateHandler()
     {
+
+
+
+
+        //checking if able to uncrouch
+        if (stateplayer == MovementState.Crouching)
+        {
+            isCeiling = Physics.CheckSphere(CeilingCheck.position, CeilingDisstance, GroundMask);
+        }
+
+        //mode - crouching
+        if (Input.GetKey(CrouchKey) || isCeiling == true)
+        {
+
+          
+            stateplayer = MovementState.Crouching;
+            Speed = CrouchSpeed;
+            transform.localScale = new Vector3(transform.localScale.x, CrouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
         //mode - sprinting
-        if (Input.GetKey(SprintKey) && isGrounded)
+        else if (Input.GetKey(SprintKey) && isGrounded && stateplayer!= MovementState.Crouching)
         {
             stateplayer = MovementState.Sprinting;
             Speed = SprintSpeed;
@@ -51,33 +89,42 @@ public class PlayerMovement : MonoBehaviour
         {
             stateplayer = MovementState.Walking;
             Speed = WalkingSpeed;
+            transform.localScale = new Vector3(transform.localScale.x, StartYScale, transform.localScale.z);
         }
         else
         {
             stateplayer = MovementState.Air;
         }
+       
     }
     
     void MovementPlayer()
     {
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float xmovement = Input.GetAxis("Horizontal");
+        float zmovement = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * xmovement + transform.forward * zmovement;
 
         characterController.Move(move * Speed * Time.deltaTime);
-
+        
+    
         //jumping
         if (Input.GetKey(JumpKey) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
         }
+        //crouching
+       
+       
+        
+       
 
     }
-
+    
     void GroundChecks()
     {
+        
         isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDisstance, GroundMask);
         //checking if you are falling
         if (isGrounded && velocity.y < 0)
@@ -88,20 +135,27 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
 
+        
+       
+
     }
 
 
-
+   
+    
 
 
 
     // Update is called once per frame
     void Update()
     {
-              
-        StateHandler();
-        MovementPlayer();
         GroundChecks();
-                 
+        StateHandler();
+       
+        MovementPlayer();
+        
+
+
+
     }
 }
